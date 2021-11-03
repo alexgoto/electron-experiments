2a15fec5c019298f6761886a3470b9e2ed6a7a5a
AlexandrGotovtsev <a.gotovcev@auto-sys.su>
2021-11-03 22:57:04 +0300

    start script added

--- view-over-view/main.js
+++ view-over-view/main.js
@@ -1,20 +1,24 @@
-const { BrowserWindow, BrowserView } = require("electron");
+const { app, BrowserWindow, BrowserView } = require("electron");
 const path = require("path");
 
 let win;
 
-win = new BrowserWindow({
-  width: 800,
-  height: 600,
-  title: "View over main view electron experiment",
-  webPreferences: {
-    devTools: true,
-    nodeIntegration: false,
-    nodeIntegrationInWorker: false,
-    nodeIntegrationInSubFrames: false,
-    contextIsolation: true,
-    enableRemoteModule: false,
-    preload: path.join(__dirname, "preload.js"),
-    disableBlinkFeatures: "Auxclick",
-  },
-});
+const createWindow = () => {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: "View over main view electron experiment",
+    webPreferences: {
+      devTools: true,
+      nodeIntegration: false,
+      nodeIntegrationInWorker: false,
+      nodeIntegrationInSubFrames: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, "preload.js"),
+      disableBlinkFeatures: "Auxclick",
+    },
+  });
+};
+
+app.on("ready", createWindow);
