4b3ecf0ed221d5844fef58299c4e8e58fa5bfa1d
AlexandrGotovtsev <a.gotovcev@auto-sys.su>
2021-11-11 13:14:46 +0300

    view over view exp is created

--- view-over-view/main.js
+++ view-over-view/main.js
@@ -4,21 +4,29 @@ const path = require("path");
 let win;
 
 const createWindow = () => {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    title: "View over main view electron experiment",
-    webPreferences: {
-      devTools: true,
-      nodeIntegration: false,
-      nodeIntegrationInWorker: false,
-      nodeIntegrationInSubFrames: false,
-      contextIsolation: true,
-      enableRemoteModule: false,
-      preload: path.join(__dirname, "preload.js"),
-      disableBlinkFeatures: "Auxclick",
-    },
-  });
+  //   win = new BrowserWindow({
+  //     width: 800,
+  //     height: 600,
+  //     title: "View over main view electron experiment",
+  //     webPreferences: {
+  //       devTools: true,
+  //       nodeIntegration: false,
+  //       nodeIntegrationInWorker: false,
+  //       nodeIntegrationInSubFrames: false,
+  //       contextIsolation: true,
+  //       enableRemoteModule: false,
+  //       preload: path.join(__dirname, "preload.js"),
+  //       disableBlinkFeatures: "Auxclick",
+  //     },
+  //   });
+
+    // win.loadURL("https://google.com");
+
+  win = new BrowserWindow({ width: 800, height: 600 });
+  const view = new BrowserView();
+  win.setBrowserView(view);
+  view.setBounds({ x: 0, y: 0, width: 600, height: 500 });
+  view.webContents.loadURL("https://google.com");
 };
 
 app.on("ready", createWindow);
